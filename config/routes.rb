eec914eb880242e462064daa66a99dedf7ef8954
Antonio Sombra <aluis.852@gmail.com>
2016-06-07 18:28:01 -0300

    parei na pg111

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   get 'store/index'
 
   resources :products
 end
   Depot::Application.routes.draw do
+  resources :line_items
+  resources :carts
     get "store/index"
     resources :products
     root 'store#index',as: 'store'
