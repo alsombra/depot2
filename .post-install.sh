6ba6cfeabc0bd8bb05e8bdf82b93412e1a957c36
Antonio Sombra <aluis.852@gmail.com>
2016-07-04 17:20:01 -0300

    set up docker dev/prod ok

--- .post-install.sh
+++ .post-install.sh
@@ -2,4 +2,4 @@ bundle install
 bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile # nao precisa em dev
